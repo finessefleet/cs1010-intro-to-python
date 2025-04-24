0464646d4cc1d30a49e1f62bf3445f8c5be651fa
AJAY AHIRWAR <154911157+ajayahirwar920@users.noreply.github.com>
2025-04-24 08:31:46 +0530

    Python Fundamentals
    This file shows the first basic Python program, the arithmetic operations.

---
+++ learners/2025CSMOOC0078/Pythonbasics.py
@@ -0,0 +1,10 @@
+#The First Arithmetic Operation Program in Python
+print("Arithematic operation on two numbers")
+a = 95
+b = 8
+result_add = a + b
+result_sub = a-b
+result_mult = a * b
+result_div = a / b
+result_intdiv = a // b #The double slashes // gives the result in interger rather than float
+print("Addition: ", result_add, "\n\nSubstraction: ",result_sub, "\n\nDivision: ",result_div,"\n\nMultiplication: ",result_mult,"\n\nDiv result in integer: ",result_intdiv)
